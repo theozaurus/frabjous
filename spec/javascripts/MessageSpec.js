1ce032731d7c20c26ae69f116933001a8dd3929d
Theo Cushion <theo@jivatechnology.com>
2012-03-16 17:59:01 +0000

    Feature: Added thread, parent_thread and correct handling of type to Message model

--- spec/javascripts/MessageSpec.js
+++ spec/javascripts/MessageSpec.js
@@ -14,6 +14,28 @@ describe("Message", function() {
       expect(m.get('to')).toBeJid('bob@bar.com');
       expect(m.get('subject')).toEqual('');
     });
+    
+    it("should default the type to normal", function(){
+      var s = parseStanza("<message to='bob@bar.com' from='alice@bar.com'><body>hello there</body></message>");
+      
+      var m = Frabjous.Store.find(Frabjous.Message, s.id());
+      expect(m.get('type')).toEqual('normal');
+    });
+    
+    it("should record thread information", function(){
+      var s = parseStanza("<message to='bob@bar.com' from='alice@bar.com'><body>hello there</body><thread>123</thread></message>");
+      
+      var m = Frabjous.Store.find(Frabjous.Message, s.id());
+      expect(m.get('thread')).toEqual('123');
+    });
+    
+    it("should record parent thread information", function(){
+      var s = parseStanza("<message to='bob@bar.com' from='alice@bar.com'><body>hello there</body><thread parent='ABC'>123</thread></message>");
+      
+      var m = Frabjous.Store.find(Frabjous.Message, s.id());
+      expect(m.get('thread')).toEqual('123');
+      expect(m.get('parent_thread')).toEqual('ABC');
+    });
   });
 
 });
--- src/xeps/message.js
+++ src/xeps/message.js
@@ -2,11 +2,13 @@
 //= require jquery
 
 Frabjous.Message = DS.Model.extend({
-  from:    DS.attr('jidString'),
-  to:      DS.attr('jidString'),
-  type:    DS.attr('string'),
-  body:    DS.attr('string'),
-  subject: DS.attr('string'),
+  from:          DS.attr('jidString'),
+  to:            DS.attr('jidString'),
+  type:          DS.attr('string'),
+  body:          DS.attr('string'),
+  subject:       DS.attr('string'),
+  thread:        DS.attr('string'),
+  parent_thread: DS.attr('string')
 });
 
 Frabjous.Parser.register("Message", function(stanza){
@@ -16,9 +18,13 @@ Frabjous.Parser.register("Message", function(stanza){
     parsed.id      = stanza.id();
     parsed.from    = stanza.from();
     parsed.to      = stanza.to();
-    parsed.type    = stanza.type();
+    parsed.type    = stanza.type() || "normal";
     parsed.subject = stanza.root().find('subject').text();
     parsed.body    = stanza.root().find('body').text();
+    
+    parsed.thread        = stanza.root().find('thread').text();
+    parsed.parent_thread = stanza.root().find('thread').attr('parent');
+    
     parsed.frabjous_type = Frabjous.Message;
     
     return parsed;
