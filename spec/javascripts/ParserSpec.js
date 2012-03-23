9a64dc465ce767d78925ef35a0790670da60edca
Theo Cushion <theo@jivatechnology.com>
2012-03-23 17:57:37 +0000

    Feature: Added code to pair error responses with existing objects

--- spec/javascripts/ParserSpec.js
+++ spec/javascripts/ParserSpec.js
@@ -13,6 +13,32 @@ describe("Parser", function(){
       expect(r).toBe(r);
     });
     
+    it("should create an object", function(){
+      var s = createStanza("<message type='chat' to='bob@bar.com' from='alice@bar.com'><body>hello there</body></message>");
+      
+      subject.handle(s);
+      
+      var e = Frabjous.Store.find(Frabjous.Message, s.id());
+      expect(e).toBeDefined();
+    });
+    
+    it("if an error is received and an object already exists it should just add an error", function(){
+      var request = createStanza("<message from='romeo@montague.net/orchard' id='ud7n1f4h' to='bar@example.org' type='chat'><body>yt?</body></message>");
+      var response = createStanza("<message from='bar@example.org' to='romeo@montague.net/orchard' id='ud7n1f4h' type='error'><error type='cancel'><remote-server-not-found xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/></error></message>");
+      
+      subject.handle(request);
+      subject.handle(response);
+      
+      var o = Frabjous.Store.find(Frabjous.Message, request.id());
+      
+      expect(o.get('from')).toBeJid('romeo@montague.net/orchard'); // The original sender
+      expect(o.get('to')).toBeJid('bar@example.org');              // The original recipient
+      
+      var error = o.get('error');
+      expect(error).toBeDefined();
+      expect(error.get('condition')).toEqual('remote-server-not-found');
+    });
+    
   });
   
 });
--- src/parser/parser.js
+++ src/parser/parser.js
@@ -5,6 +5,28 @@ Frabjous.Parser = function(){
   // Private
   var _handlers = {};
 
+  var create_or_update = function(item){
+    var frabjous_type = item.frabjous_type;
+    delete item.frabjous_type;
+    
+    Frabjous.log.debug("Parsed "+frabjous_type+":",item);
+    
+    // Consider handling errors in another way
+    var existing  = !Ember.none(Frabjous.Store.clientIdForId(frabjous_type,item.id));
+    var has_error = !Ember.none(item.error);
+    if(existing && has_error){
+      Frabjous.log.debug("Adding error to existing object "+frabjous_type+":",item.error);
+      var o = Frabjous.Store.find(frabjous_type,item.id);
+      var raw = o.toJSON();
+      raw.error = item.error;
+      return Frabjous.Store.load_and_find(frabjous_type,raw);
+    } else {
+      // No error, or no existing object
+      Frabjous.log.debug("Creating new object "+frabjous_type+":",item);
+      return Frabjous.Store.load_and_find(frabjous_type,item);
+    }
+  };
+
   return {
     // Class Methods
     handle: function(stanza){
@@ -22,14 +44,11 @@ Frabjous.Parser = function(){
       var first_result;
       for(var i in items){
         if(items.hasOwnProperty(i)){
-          var item = items[i];
-          var frabjous_type = item.frabjous_type;
-          delete item.frabjous_type;
-          Frabjous.log.debug("Parsed "+frabjous_type+":",item);
-          var result = Frabjous.Store.load_and_find(frabjous_type,item);
+          var result = create_or_update(items[i]);
           if(Ember.none(first_result)){ first_result = result; }
         }
       }
+      
       return first_result;
     },
     handlers: function(){
